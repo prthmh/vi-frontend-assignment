a380bb9f9008b4da47f4009be7bdc9779bf9772c
Prathmesh <111947619+prthmh@users.noreply.github.com>
2024-04-28 15:23:42 +0530

    Task 3 completed

--- app/_components/data_table.tsx
+++ app/_components/data_table.tsx
@@ -16,7 +16,7 @@ import {
     useReactTable,
 } from "@tanstack/react-table";
 import { DataTablePagination } from "./data-table-pagination";
-import { useState } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
 
 interface DataTableProps<TData, TValue> {
@@ -34,15 +34,54 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
 
     // TASK : Make first 2 columns (i.e. checkbox and task id) sticky
     // TASK : Make header columns resizable
+    const columnWidthRef = useRef<HTMLTableRowElement>();
+    const columnClick = (e: any) => {
+        console.log(e.target);
+        let tdElement = e.target;
+
+        do {
+            let parent = tdElement.parentNode;
+            console.log("parent", parent);
+            tdElement = parent;
+        } while (tdElement.tagName.toLowerCase() !== "td");
+        console.log(tdElement);
+        columnWidthRef.current = tdElement;
+        // tdElement.style.width = "20px";
+    };
+
+    const increaseWidth = () => {
+        const element = columnWidthRef.current;
+        if (element) {
+            const currentWidth = element.clientWidth;
+            element.style.width = `${currentWidth + 10}px`;
+        }
+    };
+    const decreaseWidth = () => {
+        const element = columnWidthRef.current;
+        if (element) {
+            const currentWidth = element.clientWidth;
+            element.style.width = `${currentWidth - 10}px`;
+        }
+    };
 
     const [checkedRow, setCheckedRow] = useState<string>();
     const selectRow = (rowId: string) => {
         setCheckedRow(rowId);
     };
-    console.log("rows", table.getRowModel().rows);
-    console.log("selected rows", checkedRow);
+    console.log(checkedRow);
     return (
         <div className="space-y-4">
+            <div className=" flex gap-3 text-white items-center">
+                <button onClick={increaseWidth} className=" bg-blue-700 text-lg px-2 rounded-md">
+                    +
+                </button>
+                <button onClick={decreaseWidth} className=" bg-red-600 text-lg px-2 rounded-md">
+                    -
+                </button>
+                <span className=" font-medium text-base text-black">
+                    (Select a column to change the column width)
+                </span>
+            </div>
             <div className="rounded-md border">
                 <Table className="custom-stick-class">
                     <TableHeader>
@@ -71,8 +110,9 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
                             table.getRowModel().rows.map((row) => (
                                 <TableRow
                                     key={row.id}
+                                    onClick={(e) => columnClick(e)}
                                     data-state={row.getIsSelected() && "selected"}
-                                    className={cn({
+                                    className={cn("overflow-hidden", {
                                         "bg-slate-300": checkedRow === row.id,
                                     })}
                                 >
@@ -81,7 +121,10 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
                                         <input
                                             type="checkbox"
                                             checked={checkedRow === row.id}
-                                            onChange={() => selectRow(row.id)}
+                                            onChange={(e) => {
+                                                e.stopPropagation();
+                                                selectRow(row.id);
+                                            }}
                                         />
                                     </TableCell>
 
